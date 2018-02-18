3ade9568c603f638ef2cc12e4c8a79579297b769
may <may.karaage@gmail.com>
2018-02-18 17:02:18 +0000

    require login to show favorites

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :favoposts
+  ]
   def index
     @users = User.all.page(params[:page])
   end
